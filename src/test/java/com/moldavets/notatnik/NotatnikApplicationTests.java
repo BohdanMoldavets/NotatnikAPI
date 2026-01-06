a9937139603ca3da13c799fd321ff0c2bbf48f54
BohdanMoldavets <143205638+BohdanMoldavets@users.noreply.github.com>
2026-01-06 11:45:25 +0100

    feat: initial commit

---
+++ src/test/java/com/moldavets/notatnik/NotatnikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.moldavets.notatnik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotatnikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
